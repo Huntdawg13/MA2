fb42cdbb3bad1f8fcfc488c5c402dba6208edf00
Huntdawg13 <huntermcclure13@gmail.com>
2020-10-12 23:52:45 -0700

    Finished assignment

---
+++ Btree.java
@@ -0,0 +1,55 @@
+// Java program to illustrate the sum of two numbers 
+
+// A BTree 
+class Btree { 
+	public BTreeNode root; // Pointer to root node 
+	public int t; // Minimum degree 
+
+	// Constructor (Initializes tree as empty) 
+	Btree(int t) { 
+		this.root = null; 
+		this.t = t; 
+	} 
+
+	// function to traverse the tree 
+	public void traverse() { 
+		if (this.root != null) 
+			this.root.traverse(); 
+		System.out.println(); 
+	} 
+
+	// function to search for a key
+	public BTreeNode search(int k) { 
+		if (this.root == null) 
+			return null; 
+		else
+			return this.root.search(k); 
+	}
+   
+   public void insert(int k)
+   {
+      if(root == null)
+      {
+         root = new BTreeNode(t, true);
+         root.keys[0] = k;
+         root.n = 1;
+      }
+      else
+      {
+         if(root.n == 2*t-1)
+         {
+            BTreeNode s = new BTreeNode(t, false);
+            
+            s.C[0] = root;
+            
+            s.splitChild(0, root);
+            
+            root = s;
+         }
+         else
+            root.insertNonFull(k);
+         
+         
+      }
+   } 
+} 
